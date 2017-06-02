739c3ce0648a0c49388ebf4d2e6bb1ad77d321ce
Morten Markussen Lang <lycilph@gmail.com>
2017-06-02 20:26:45 +0200

    Fixed a shutdown bug

--- Mockups/WebView.xaml.cs
+++ Mockups/WebView.xaml.cs
@@ -7,12 +7,18 @@ namespace Mockups
         public WebView()
         {
             InitializeComponent();
+            DataContext = this;
         }
 
         private void BackClick(object sender, RoutedEventArgs e)
         {
             var window = Application.Current.MainWindow as MainWindow;
             window.ShowBooksView();
+        }
+
+        private void HomeClick(object sender, RoutedEventArgs e)
+        {
+            WebBrowser.Address = "http://www.google.com";
         }
     }
 }
