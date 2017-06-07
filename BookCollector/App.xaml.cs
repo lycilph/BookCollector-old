0d0ed0a7fba2c5e31566b544ab78a2c68955a3ce
Morten Markussen Lang <lycilph@gmail.com>
2017-06-07 08:13:15 +0200

    Added the web screen

--- BookCollector/App.xaml.cs
+++ BookCollector/App.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Reflection;
+using System.Windows;
 using AutoMapper;
 using BookCollector.Domain;
 using BookCollector.Domain.Configuration;
 using BookCollector.Framework.Logging;
+using CefSharp;
 using Ninject;
 
 namespace BookCollector
@@ -21,6 +24,8 @@ namespace BookCollector
 
         private void ApplicationStartup(object sender, StartupEventArgs e)
         {
+            // Setup CEF
+            SetupCEF();
             // Configure ninject dependency injection
             kernel = new StandardKernel(new ApplicationModule());
             // Configure Automapper
@@ -32,7 +37,21 @@ namespace BookCollector
 
         private void ApplicationExit(object sender, ExitEventArgs e)
         {
+            Cef.Shutdown();
+        }
 
+        private void SetupCEF()
+        {
+            var path = Assembly.GetExecutingAssembly().Location;
+            var dir = Path.GetDirectoryName(path);
+
+            var settings = new CefSettings()
+            {
+                //By default CefSharp will use an in-memory cache, you need to specify a Cache Folder to persist data
+                CachePath = Path.Combine(dir, "CefSharp"),
+            };
+            //Perform dependency check to make sure all relevant resources are in our output directory.
+            Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
         }
     }
 }
--- BookCollector/Screens/Web/WebViewModel.cs
+++ BookCollector/Screens/Web/WebViewModel.cs
@@ -7,6 +7,20 @@ namespace BookCollector.Screens.Web
 {
     public class WebViewModel : ScreenBase
     {
+        private string _Address;
+        public string Address
+        {
+            get { return _Address; }
+            set { this.RaiseAndSetIfChanged(ref _Address, value); }
+        }
+
+        private ReactiveCommand _HomeCommand;
+        public ReactiveCommand HomeCommand
+        {
+            get { return _HomeCommand; }
+            set { this.RaiseAndSetIfChanged(ref _HomeCommand, value); }
+        }
+
         private ReactiveCommand _BackCommand;
         public ReactiveCommand BackCommand
         {
@@ -17,7 +31,8 @@ namespace BookCollector.Screens.Web
         public WebViewModel(IEventAggregator event_aggregator)
         {
             DisplayName = ScreenNames.WebName;
-
+            Address = "http://www.goodreads.com";
+            HomeCommand = ReactiveCommand.Create(() => Address = "http://www.goodreads.com");
             BackCommand = ReactiveCommand.Create(() => event_aggregator.Publish(ApplicationMessage.NavigateToMessage(ScreenNames.BooksName)));
         }
     }
